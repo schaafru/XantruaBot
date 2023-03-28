68c9e2dc04c489e978819081cd7c3b633905e040
Russell Schaaf <schaafru@oregonstate.edu>
2023-03-27 17:54:26 -0700

    fixed spacing

--- bot.js
+++ bot.js
@@ -46,21 +46,21 @@ function onMessageHandler(target, context, msg, self) {
     case '!sweeteggs':
       say(target, '@Zylanth\'s famous sweet egg recipe: Crack two eggs in a bowl. Whisk in 1/4 cup Sweet Cream International Delight. Heat oil in pan over medium heat. Pour egg mixture into pan. Gently fold until light and fluffy. Enjoy with an ice cold Steel Reserve');
       break;
-      case '!roll d4':
-      case '!roll d6':
-      case '!roll d8':
-      case '!roll d10':
-      case '!roll d20':
-      case '!roll d100':
-        const sizeStr = command.split(' ')[1];
-        const side = parseInt(sizeStr.substring(1));
-        if (!isNaN(side)) {
-          const result = Math.floor(Math.random() * side) + 1;
-          say(target, `@${context.username} rolled a ${result}`);
-        } else {
-          say(target, `@${context.username} Invalid roll command. Please specify the number of sides on the die (e.g. !roll d6)`);
-        }
-        break;
+    case '!roll d4':
+    case '!roll d6':
+    case '!roll d8':
+    case '!roll d10':
+    case '!roll d20':
+    case '!roll d100':
+      const sizeStr = command.split(' ')[1];
+      const side = parseInt(sizeStr.substring(1));
+      if (!isNaN(side)) {
+        const result = Math.floor(Math.random() * side) + 1;
+        say(target, `@${context.username} rolled a ${result}`);
+      } else {
+        say(target, `@${context.username} Invalid roll command. Please specify the number of sides on the die (e.g. !roll d6)`);
+      }
+      break;
     case '!roll':
       say(target, `@${context.username} to use the roll feature, type the command followed by either d4, d8, d10, d20, or d100.`);
       break;
