fd3a6c229f2876e8c94181e07b5e4a375184e09e
Russell Schaaf <schaafru@oregonstate.edu>
2023-03-27 17:53:52 -0700

    refactored code

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 const tmi = require('tmi.js');
 
-// Define configuration options
 const opts = {
   identity: {
     username: 'xantruabot',
@@ -11,121 +10,89 @@ const opts = {
   ]
 };
 
-// Create a client with our options
 const client = new tmi.client(opts);
 
-// Register our event handlers (defined below)
 client.on('message', onMessageHandler);
 client.on('connected', onConnectedHandler);
 
-// Connect to Twitch:
 client.connect();
 
-// Called every time a message comes in
-function onMessageHandler (target, context, msg, self) {
-  if (self) { return; } // Ignore messages from the bot
+function onMessageHandler(target, context, msg, self) {
+  if (self) {
+    return;
+  }
 
-  // Remove whitespace from chat message
-  const commandName = msg.trim();
+  const command = msg.trim();
 
-  if (commandName === '!russell') {
-    client.say(target, 'VoHiYo')
-    console.log(`* Executed ${commandName} command`);
-  } 
-  else if (commandName === '!alex') {
-    client.say(target, 'Kreygasm')
-    console.log(`* Executed ${commandName} command`);
+  switch (command) {
+    case '!russell':
+      say(target, 'VoHiYo');
+      break;
+    case '!alex':
+      say(target, 'Kreygasm');
+      break;
+    case '!kira':
+      say(target, 'Deez nuts');
+      break;
+    case '!penislength':
+      const size = penisSize(context.username);
+      const length = size === 1 ? 'inch' : 'inches';
+      say(target, `@${context.username}'s penis length is ${size} ${length}`);
+      break;
+    case '!derek':
+      const derekPercentage = context.name === 'Zylanth' ? '123%' : `${derek(context.username)}%`;
+      say(target, `@${context.username} is ${derekPercentage} derek HeyGuys`);
+      break;
+    case '!sweeteggs':
+      say(target, '@Zylanth\'s famous sweet egg recipe: Crack two eggs in a bowl. Whisk in 1/4 cup Sweet Cream International Delight. Heat oil in pan over medium heat. Pour egg mixture into pan. Gently fold until light and fluffy. Enjoy with an ice cold Steel Reserve');
+      break;
+      case '!roll d4':
+      case '!roll d6':
+      case '!roll d8':
+      case '!roll d10':
+      case '!roll d20':
+      case '!roll d100':
+        const sizeStr = command.split(' ')[1];
+        const side = parseInt(sizeStr.substring(1));
+        if (!isNaN(side)) {
+          const result = Math.floor(Math.random() * side) + 1;
+          say(target, `@${context.username} rolled a ${result}`);
+        } else {
+          say(target, `@${context.username} Invalid roll command. Please specify the number of sides on the die (e.g. !roll d6)`);
+        }
+        break;
+    case '!roll':
+      say(target, `@${context.username} to use the roll feature, type the command followed by either d4, d8, d10, d20, or d100.`);
+      break;
+    default:
+      console.log(`* Unknown command ${command}`);
   }
-  else if (commandName === '!kira') {
-    client.say(target, 'Deez nuts')
-    console.log(`* Executed ${commandName} command`);
-  }
-  else if (commandName === '!penislength') {
-    const num = penisSize(context.username);
-    if (num == 1)  {
-      client.say(target, `@${context.username}'s penis length is ${num} inch`)
-    }
-    else {
-      client.say(target, `@${context.username}'s penis length is ${num} inches`)
-    }
-    console.log(`* Executed ${commandName} command`);
-  }
-  else if (commandName === '!derek') {
-    if (context.name == 'Zylanth') {
-      client.say(target, `@${context.username} is 123% derek HeyGuys`);
-    }
-    else {
-      const num = derek(context.username);
-      client.say(target, `@${context.username} is ${num}% derek HeyGuys`);
-    }
-    console.log(`* Executed ${commandName} command`);
-  }
-  else if (commandName === '!sweeteggs') {
-    client.say(target, `@Zylanth\'s famous sweet egg recipe: 
-    Crack two eggs in a bowl. 
-    Whisk in 1/4 cup Sweet Cream International Delight. 
-    Heat oil in pan over medium heat. 
-    Pour egg mixture into pan. 
-    Gently fold until light and fluffy. 
-    Enjoy with an ice cold Steel Reserve`)
-    console.log(`* Executed ${commandName} command`);
-  }
-  else if (commandName == '!roll d4') {
-    num = Math.floor(Math.random() * 4) + 1;
-    client.say(target, `@${context.username} rolled a ${num}!`);
-    console.log(`* Executed ${commandName} command`);
-  }
-  else if (commandName === '!roll d6') {
-    num = Math.floor(Math.random() * 6) + 1;
-    client.say(target, `@${context.username} rolled a ${num}!`);
-    console.log(`* Executed ${commandName} command`);
-  }
-  else if (commandName === '!roll d8') {
-    num = Math.floor(Math.random() * 8) + 1;
-    client.say(target, `@${context.username} rolled a ${num}`);
-    console.log(`* Executed ${commandName} command`);
-  }
-  else if (commandName === '!roll d10') {
-    num = Math.floor(Math.random() * 10) + 1;
-    client.say(target, `@${context.username} rolled a ${num}`)
-  }
-  else if (commandName === '!roll d20') {
-    num = Math.floor(Math.random() * 20) + 1;
-    client.say(target, `@${context.username} rolled a ${num}`);
-    console.log(`* Executed ${commandName} command`);
-  }
-  else if (commandName === '!roll d100') {
-    num = Math.floor(Math.random() * 100) + 1;
-    client.say(target, `@${context.username} rolled a ${num}`);
-    console.log(`* Executed ${commandName} command`);
-  }
-  else if (commandName === '!roll'){
-    client.say(target, `@${context.username} to use the roll feature, type the command followed by either d4, d8, d10, d20, or d100.`);
-  }
-  else {
-    console.log(`* Unknown command ${commandName}`);
-  }
+  
+  console.log(`* Executed ${command} command`);
+}
+
+function say(target, message) {
+  client.say(target, message);
 }
 
 function penisSize(username) {
-  var userNumber = 0;
-  for (let i= 0; i < username.length; i++) {
+  let userNumber = 0;
+  for (let i = 0; i < username.length; i++) {
     userNumber += username.charCodeAt(i);
     userNumber = (userNumber % 12) + 1;
   }
   return userNumber;
 }
- 
+
 function derek(username) {
-  var userNumber = 0;
-  for (let i= 0; i < username.length; i++) {
+  let userNumber = 0;
+  for (let i = 0; i < username.length; i++) {
     userNumber += username.charCodeAt(i);
     userNumber = (userNumber % 99) + 1;
   }
   return userNumber;
 }
 
-// Called every time the bot connects to Twitch chat
-function onConnectedHandler (addr, port) {
+function onConnectedHandler(addr, port) {
   console.log(`* Connected to ${addr}:${port}`);
 }
